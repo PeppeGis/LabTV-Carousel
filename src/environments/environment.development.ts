139ef0cde6a77ed2d1e9c2872531471843aaa260
PeppeGis <giuseppe_gisotti@hotmail.it>
2023-07-10 23:43:19 +0200

    gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    basicUrlMovie: 'https://api.themoviedb.org/3/movie/',
+    basicUrlSearch: 'https://api.themoviedb.org/3/search/movie',
+    apikey: '46b43a7d1798082a4881e58c8a10dc1d'
+};
